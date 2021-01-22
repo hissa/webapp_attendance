4546dbd8a429d37185bc3350be846183646a76e3
石井貴啓 <takahiro0141.0704@gmail.com>
2021-01-22 12:25:18 +0900

    first commit

--- main.py
+++ main.py
@@ -43,5 +43,28 @@ def input_attendance():
 
     return render_template("input.html", students = [])
 
+@app.route('/history/list')
+def history_list():
+    db = Database()
+    sql = 'select distinct date from lessons order by date desc'
+    data = db.query(sql)
+
+    datas = list()
+    
+    for index, item in enumerate(data):
+        sql = "select id, name from lessons where date = '" + str(item[0]) + "' order by koma"
+        lessons = db.query(sql)
+        datas.append([item[0]])
+        for lesson in lessons:
+            datas[index].append([lesson[0], lesson[1]])
+    return render_template("history_list.html", data = datas)
+@app.route('/absence_list/<pk>')
+def absence_students_list(pk):
+    db = Database()
+    sql = 'select number, name from students left outer join attendances on number = student_number where lesson_id = {} and is_attended=1'
+    sql = sql.format(pk)
+    data = db.query(sql)
+    return render_template("history/absenceList.html", data = data)
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8000, threaded=True)
